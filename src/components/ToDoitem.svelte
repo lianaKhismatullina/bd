2e9ee30cce41a7132eff69fd2cbfbb4ea6ad49d5
Liana <Liana@wrerw.rr>
2023-10-22 15:54:38 +0500

    first commit

---
+++ src/components/ToDoitem.svelte
@@ -0,0 +1,55 @@
+<script>
+    export let id;
+    export let text;
+    export let isDone;
+    import { createEventDispatcher } from "svelte";
+
+
+    const dispatch = createEventDispatcher();
+</script>
+
+<div class="todo-item" class:todo-item_done={isDone}>
+    <input
+     class="todo-item_check" 
+     type="checkbox" 
+     on:change={dispatch("change", {id})} 
+     checked={isDone}
+    />
+    <span class="todo-item_text">{text}</span>
+    <button class="todo-item_btn" on:click={dispatch("remove", {id})}>Удалить</button>
+</div>
+
+
+<style>
+    .todo-item{
+        display:flex;
+        align-items: center;
+        justify-content: space-between;
+        padding: 15PX;
+        background: rgb(144, 110, 255);
+        border-radius: 10px;
+    }
+    .todo-item_done {
+        background: rgb(94, 13, 0);
+        text-decoration: line-through;
+    }
+    .todo-item_done .todo-item_text {
+        text-decoration: line-through;
+    }
+    .todo-item_check {
+        width: 20px;
+        font-size:20px;
+    }
+    .todo-item_text {
+        flex-grow: 1;
+    }
+    .todo-item_btn{
+        border: none;
+        border-radius: 10px;
+        background-color: rgb(255, 245, 172);
+        padding: 10px;
+        min-width: 100px;
+
+
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	import ToDoApp from "../components/ToDoApp.svelte";
+</script>
+<div class="wrapper">
+    <ToDoApp/>
+</div>
+<style>
+    .wrapper {
+        height: 100vp;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
